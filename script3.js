9cc19912e8492d35ebaf418ce157f7f80d277e10
Nate Macarelli <33464190+natemac47@users.noreply.github.com>
2024-02-14 15:59:23 -0500

    Update script3.js

--- script3.js
+++ script3.js
@@ -12,19 +12,24 @@ supportGr.query();
 while (supportGr.next()) {
     // Set the sproutabout_user field to true
     supportGr.setValue('sproutabout_user', true);
-    
+
+    if (supportGr.caller_id.u_sa_contact_id.nil()) {
+        // If the u_sa_contact_id field is empty, skip to the next record
+        continue;
+    }
+
     // Initialize a GlideRecord instance for the contact table
     var contactGr = new GlideRecord('x_lecg_sproutabout_child_contact');
-    
-    // Adjust the query to match contact_id in the contact table with caller_id in the support table
-    contactGr.addQuery('contact_id', supportGr.caller_id.toString());
+
+    // Adjust the query to match u_contact_id_string in the contact table with caller_id.u_sa_contact_id in the support table
+    contactGr.addQuery('u_contact_id_string', supportGr.caller_id.u_sa_contact_id);
     contactGr.query();
-    
+
     if (contactGr.next()) {
         // If a matching contact record is found, populate the sproutabout_caller field with its sys_id
         supportGr.setValue('sproutabout_caller', contactGr.sys_id.toString());
     }
-    
+
     // Update the support record with the changes
     supportGr.update();
 }
