8269c523ac222bfab8f2ff0369f83261d50df50f
Nate Macarelli <33464190+natemac47@users.noreply.github.com>
2024-02-29 22:30:11 -0500

    Update script2.2.js

--- script2.2.js
+++ script2.2.js
@@ -1,28 +1,20 @@
-//COPIES FIELDS FROM SYS USER TO SPROUTABOUT M2M RECORDS.
-// Query the x_lecg_sproutabout_child_contact table for records where child_id is not empty
-var childContactGr = new GlideRecord('x_lecg_sproutabout_child_contact');
-//childContactGr.addNotNullQuery('child_id'); // Add condition for child_id to not be null
-childContactGr.query(); // Execute the query
+var childRecord = new GlideRecord('x_lecg_sproutabout_child_contact');
+childRecord.query();
+while (childRecord.next()) {
+    var contactRef = childRecord.contact_id.getRefRecord();
 
-while (childContactGr.next()) {
-    // Get the contact_id, which is a reference to a sys_user record
-    var contactId = childContactGr.getValue('contact_id');
+    var newContact = new GlideRecord('x_lecg_sproutabout_contact');
+    newContact.initialize();
 
-    if (contactId) {
-        // Query the sys_user table for the specific user
-        var userGr = new GlideRecord('sys_user');
-        if (userGr.get(contactId)) { // Successfully retrieved the sys_user record
-            // Update the x_lecg_sproutabout_child_contact record with fields from sys_user
-            childContactGr.setValue('guardian_id', userGr.getValue('u_guardian_id'));
-            childContactGr.setValue('u_contact_id_string', userGr.getValue('u_sa_contact_id'));
-            childContactGr.setValue('family_id', userGr.getValue('u_family_id'));
-            childContactGr.setValue('name', userGr.getValue('name'));
-            childContactGr.setValue('mobile_phone', userGr.getValue('mobile_phone'));
-            childContactGr.setValue('email', userGr.getValue('email'));
-
-
-            // Update the record
-            childContactGr.update();
-        }
-    }
+    newContact.email = contactRef.getValue('email');
+    newContact.first_name = contactRef.getValue('first_name');
+    newContact.last_name = contactRef.getValue('last_name');
+    newContact.name = contactRef.getValue('name');
+    newContact.mobile_phone = contactRef.getValue('mobile_phone');
+    newContact.guardian_id = contactRef.getValue('u_guardian_id');
+    newContact.family_id = contactRef.getValue('u_family_id');
+    newContact.contact_id_string = contactRef.getValue('u_sa_contact_id');
+    newContact.contact_id = childRecord.sys_id;
+    
+    newContact.insert();
 }
