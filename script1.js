05d6ab2a78ab8f121c3d4673ef0a26a50fc4bcf9
Nate Macarelli <33464190+natemac47@users.noreply.github.com>
2024-03-10 17:56:01 -0400

    Update script1.js

--- script1.js
+++ script1.js
@@ -1,29 +1,13 @@
 //SETS USER RECORDS ON SYS_USER WHERE CONTACT_ID IS FILLED IN TO SPROUTABOUT CONTACT = TRUE. TAKES APPROX. 8-10 HRS
-// Define the table you want to update
-var tableName = 'sys_user'; // Replace 'example_table' with your actual table name
-
-// Create a GlideRecord instance for the specified table
+var tableName = 'sys_user'; 
 var gr = new GlideRecord(tableName);
-
-// Add a query to find records where u_sa_contact_id is not null or not empty
 gr.addQuery('u_sa_contact_id', '!=', '');
-
-// Query the database
 gr.query();
-
-// Initialize a counter for updated records
 var count = 0;
-
-// Loop through the returned records
 while (gr.next()) {
-    // Set u_sproutabout_contact to true
     gr.u_sproutabout_contact = true;
     gr.u_marked_for_deletion = true;
-
-    // Update the record
     gr.update();
-    
-    // Increment the counter
     count++;
 }
 
